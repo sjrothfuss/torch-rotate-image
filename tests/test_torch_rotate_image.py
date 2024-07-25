eee4863b478ce8956e07452002d4aa73f12ef61d
Spencer J Rothfuss <spencer.j.rothfuss@vanderbilt.edu>
2024-07-25 06:35:28 -0500

    Adding tests

--- tests/test_torch_rotate_image.py
+++ tests/test_torch_rotate_image.py
@@ -2,11 +2,16 @@ import torch
 from torch_rotate_image import rotate_image_2d
 
 
-def test_rotate_image_2d():
+def test_rotate_image_2d_shape():
     image = torch.rand(28, 28)
     angles = torch.tensor([0.0, 90.0])
     rotated_image = rotate_image_2d(image, angles)
     assert rotated_image.shape == (2, 28, 28)
 
 
-test_rotate_image_2d()
+def test_rotate_image_2d_rotation():
+    image = torch.zeros(28, 28)
+    image[:14, :] = 1  # image is hafl black, half white
+    angles = torch.tensor([0.0, 90.0])
+    rotated_image = rotate_image_2d(image, angles)
+    assert rotated_image[0] == image and rotated_image[1] == image.flip(0)
