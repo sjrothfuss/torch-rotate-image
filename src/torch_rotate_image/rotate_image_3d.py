c3b2bcf8fba51cd44d5443ffcb955b4596eb92c7
Spencer J Rothfuss <spencer.j.rothfuss@vanderbilt.edu>
2025-03-12 10:52:21 -0500

    Add framework for 3D rotation

--- src/torch_rotate_image/rotate_image_3d.py
+++ src/torch_rotate_image/rotate_image_3d.py
@@ -1,2 +1,12 @@
+"""WIP module for 3D tensor rotation."""
+
+import torch
+import einops
+
+
 def rotate_image_3d():
     raise NotImplementedError("Not implemented yet")
+
+
+def _invert_3d_rotation_matrix(matrix: torch.Tensor) -> torch.Tensor:
+    return torch.linalg.inv(matrix)
