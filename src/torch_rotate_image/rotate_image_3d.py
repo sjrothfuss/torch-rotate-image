bc538e453d07823e5eff008d1d739a822abde0d6
Spencer J Rothfuss <spencer.j.rothfuss@vanderbilt.edu>
2025-03-12 10:34:33 -0500

    Reconciling test conflicts

--- src/torch_rotate_image/rotate_image_3d.py
+++ src/torch_rotate_image/rotate_image_3d.py
@@ -1,2 +1,12 @@
+"""WIP 3D tensor rotation module."""
+
+import torch
+import einops
+
+
 def rotate_image_3d():
     raise NotImplementedError("Not implemented yet")
+
+
+def _invert_rotation_matrix(matrix: torch.Tensor) -> torch.Tensor:
+    return torch.linalg.inv(matrix)
